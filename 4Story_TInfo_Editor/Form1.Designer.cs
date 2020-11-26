4cbe9dc97a889800dc31aa727df3051c57364636
Agnares <60239119+Agnares@users.noreply.github.com>
2020-11-26 20:29:24 +0100

    Add files via upload

---
+++ 4Story_TInfo_Editor/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace _4Story_TInfo_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.removeItemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.TextColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.StatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.searchToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.addToolStripMenuItem,
+            this.removeItemToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.fileToolStripMenuItem.Text = "Menu";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.addToolStripMenuItem.Text = "Add to list";
+            this.addToolStripMenuItem.Click += new System.EventHandler(this.addToolStripMenuItem_Click);
+            // 
+            // removeItemToolStripMenuItem
+            // 
+            this.removeItemToolStripMenuItem.Name = "removeItemToolStripMenuItem";
+            this.removeItemToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.removeItemToolStripMenuItem.Text = "Remove from list";
+            this.removeItemToolStripMenuItem.Click += new System.EventHandler(this.removeItemToolStripMenuItem_Click);
+            // 
+            // searchToolStripMenuItem
+            // 
+            this.searchToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1});
+            this.searchToolStripMenuItem.Name = "searchToolStripMenuItem";
+            this.searchToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.searchToolStripMenuItem.Text = "Search";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 23);
+            this.toolStripTextBox1.Tag = "";
+            this.toolStripTextBox1.DoubleClick += new System.EventHandler(this.toolStripTextBox1_DoubleClick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.IntegralHeight = false;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(185, 404);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TextColumn});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dataGridView1.Location = new System.Drawing.Point(187, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(613, 404);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            this.dataGridView1.UserAddedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridView1_UserAddedRow);
+            this.dataGridView1.UserDeletedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridView1_UserDeletedRow);
+            // 
+            // TextColumn
+            // 
+            this.TextColumn.HeaderText = "Text";
+            this.TextColumn.Name = "TextColumn";
+            this.TextColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.TextColumn.Width = 500;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(39, 17);
+            this.StatusLabel.Text = "Status";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 404);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "TINFO_EDITOR_AGNARES";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TextColumn;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removeItemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem searchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ 4Story_TInfo_Editor/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32;
+
+namespace _4Story_TInfo_Editor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            StatusLabel.Text = "Status";
+        }
+
+        public List<DetailInfo> Data = new List<DetailInfo>();
+
+        public class DetailInfo
+        {
+            public uint m_dwID { get; set; }
+            public byte m_bCount { get; set; }
+            public List<string> m_pTEXT = new List<string>();
+        }
+
+        public void WriteUInt16Length(BinaryWriter bw, int length)
+        {
+            bw.Write(byte.MaxValue);
+            bw.Write((ushort)length);
+        }
+
+        public void WriteByteLength(BinaryWriter bw, int length)
+        {
+            bw.Write((byte)length);
+        }
+
+        public void ReadBinary(Stream stream)
+        {
+            Data.Clear();
+            listBox1.Items.Clear();
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+
+            StatusLabel.Text = "Loading data...";
+            Application.DoEvents();
+
+            BinaryReader reader = new BinaryReader(stream, Encoding.Default);
+            ushort wCount = reader.ReadUInt16();
+            for (ushort i = 0; i < wCount; i++)
+            {
+                DetailInfo info = new DetailInfo();
+                info.m_dwID = reader.ReadUInt32();
+                info.m_bCount = reader.ReadByte();
+                for (byte j = 0; j < info.m_bCount; j++)
+                {
+                    ushort length = reader.ReadByte();
+                    if (length >= byte.MaxValue)
+                        length = reader.ReadUInt16();
+                    info.m_pTEXT.Add(Encoding.Default.GetString(reader.ReadBytes(length)));
+                }
+                Data.Add(info);
+                listBox1.Items.Add(i);
+            }
+
+            StatusLabel.Text = "Data loaded.";
+            stream.Close();
+        }
+
+        public void SaveBinary(Stream stream)
+        {
+            StatusLabel.Text = "Saving data...";
+            Application.DoEvents();
+
+            BinaryWriter writer = new BinaryWriter(stream, Encoding.Default);
+            writer.Write((ushort)Data.Count);
+            for(int i = 0; i < Data.Count; i++)
+            {
+                writer.Write(Data[i].m_dwID);
+                writer.Write(Data[i].m_bCount);
+                for (int j = 0; j < Data[i].m_pTEXT.Count; j++)
+                {
+                    int length = Data[i].m_pTEXT[j].Length;
+                    if (length >= byte.MaxValue)
+                        WriteUInt16Length(writer, length);
+                    else
+                        WriteByteLength(writer, length);
+                    writer.Write(Encoding.Default.GetBytes(Data[i].m_pTEXT[j]));
+                }
+            }
+
+            StatusLabel.Text = "Data saved.";
+            stream.Close();
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "All files (*.*)|*.*";
+            ofd.FilterIndex = 2;
+            ofd.RestoreDirectory = true;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                ReadBinary(ofd.OpenFile());
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "All files (*.*)|*.*";
+            sfd.FilterIndex = 2;
+            sfd.RestoreDirectory = true;
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                SaveBinary(sfd.OpenFile());
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                dataGridView1.Rows.Clear();
+                dataGridView1.Refresh();
+                for (int i = 0; i < Data[listBox1.SelectedIndex].m_pTEXT.Count; i++)
+                {
+                    int index = dataGridView1.Rows.Add();
+                    dataGridView1.Rows[index].Cells[dataGridView1.Columns["TextColumn"].Index].Value = Data[listBox1.SelectedIndex].m_pTEXT[i];
+                }
+            }
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            for (int i = 0; i < Data[listBox1.SelectedIndex].m_pTEXT.Count; i++)
+            {
+                Data[listBox1.SelectedIndex].m_pTEXT[i] = (string)dataGridView1.Rows[i].Cells[dataGridView1.Columns["TextColumn"].Index].Value;
+            }
+        }
+
+        private void dataGridView1_UserAddedRow(object sender, DataGridViewRowEventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                Data[listBox1.SelectedIndex].m_pTEXT.Add("");
+                Data[listBox1.SelectedIndex].m_bCount += 1;
+            }
+        }
+
+        private void dataGridView1_UserDeletedRow(object sender, DataGridViewRowEventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                Data[listBox1.SelectedIndex].m_pTEXT.RemoveAt(dataGridView1.CurrentCell.RowIndex);
+                Data[listBox1.SelectedIndex].m_bCount -= 1;
+            }
+        }
+
+        private void addToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DetailInfo info = new DetailInfo();
+            info.m_dwID = Data.Count > 0 ? Data.Last().m_dwID + 1 : 0;
+            info.m_bCount = 0;
+            Data.Add(info);
+            listBox1.Items.Clear();
+            for (int i = 0; i < Data.Count; i++)
+                listBox1.Items.Add(i);  
+        }
+
+        private void removeItemToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Data.RemoveAt(listBox1.SelectedIndex);
+            listBox1.Items.Clear();
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+            for (int i = 0; i < Data.Count; i++)
+                listBox1.Items.Add(i);
+        }
+
+        private void toolStripTextBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if(toolStripTextBox1.TextLength > 0)
+            {
+                List<int> ItemsFound = new List<int>();
+                ItemsFound.Clear();
+                for (int i = 0; i < Data.Count; i++)
+                    for (int j = 0; j < Data[i].m_pTEXT.Count; j++)
+                        if (Data[i].m_pTEXT[j].Contains(toolStripTextBox1.Text))
+                            if(!ItemsFound.Contains(i))
+                                ItemsFound.Add(i);
+
+                string strItems = "";
+                for (int i = 0; i < ItemsFound.Count; i++)
+                    strItems += "Item: " + ItemsFound[i].ToString() + "\r\n";
+
+                MessageBox.Show(strItems);
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            int width = ClientRectangle.Width / 2;
+            listBox1.Left = 0;
+            listBox1.Width = width / 2;
+            dataGridView1.Width = width + listBox1.Width;
+        }
+    }
+}
